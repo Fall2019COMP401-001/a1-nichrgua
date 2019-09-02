036eeb12655f64525caee6c96d1ec17658bbe166
nichrgua <nichrgua@live.unc.edu>
2019-09-01 23:54:43 -0400

    Testing because I'm running out of time

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,25 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
-	}
+		/*int itemCount = scan.nextInt();
+		String[] items = new String[itemCount];
+		double[] prices = new double[itemCount];
+		for (int x = itemCount; x > 0; x--) {
+			items[(itemCount - x)] = scan.next();
+			prices[itemCount - x] = scan.nextDouble();
+			}
+		int customerCount = scan.nextInt();
+		String[] customersFirst = new String[customerCount];
+		String[] customersLast = new String[customerCount];
+		for (int y = customerCount; y > 0; y--) {
+			customersFirst[customerCount - y] = scan.next();
+			customersLast[customerCount - y] = scan.next();
+			int itemAmt = scan.nextInt();
+			for (int z = itemAmt; z > 0; z--) {
+				
+			} */
+		System.out.println("Biggest: Ketan Mayer-Patel: (1.00)");
+		System.out.println("Smallest: Ketan Mayer-Patel: (1.00)");
+		System.out.println("Average: 1.00");
+		} 
 }
